11fb9126dbef19edefbb9e15a6722ed01c8332ea
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-02 23:43:56 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/consensusalgo/main.go
@@ -0,0 +1,23 @@
+// cmd/consensusalgo/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"consensusalgo/internal/consensusalgo"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := consensusalgo.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
